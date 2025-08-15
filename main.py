29ff74af842905eb5957f2faf728c1e4f33b4166
NOXUS <morbiusfell@gmail.com>
2025-08-16 00:22:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from hacksim import start_hack
+
+if __name__ == "__main__":
+    print("=== HackSim: Hacker Simulation Terminal ===")
+    target_ip = input("Hedef IP adresini gir: ")
+    start_hack(target_ip)
